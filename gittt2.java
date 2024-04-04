68d083a1b8a8c707b6c2318cf72c9a9cd516b013
Renurai100 <rairenu345@gmail.com>
2024-04-04 11:08:22 +0530

    My second code.

---
+++ gittt2.java
@@ -0,0 +1,9 @@
+public class progm1 
+{
+public static void main(String[] args) 
+{
+System.out.println("Hello,welcome to the class.");
+System.out.println("Hello,welcome to the second class.");
+
+}
+}
