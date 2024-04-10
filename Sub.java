832158aeb0f1a1e35f4aed0e3ac5405052b52534
Renurai100 <rairenu345@gmail.com>
2024-04-10 21:22:11 +0530

    evenodd1prgm

---
+++ Sub.java
@@ -0,0 +1,33 @@
+class Sum
+{
+public static void main(String[]args)
+	{
+	int a=20;
+	int b=20;
+	int c=a+b;
+	System.out.println(c);
+	}
+}
+
+class Sub
+{
+public static void main(String[]args)
+	{
+	int a=20;
+	int b=20;
+	int c=a-b;
+	System.out.println(c);
+	}
+}
+
+ class Multi
+{
+	public static void main(String[]args)
+	{
+	int a=20;
+	int b=20;
+	int c=a*b;
+	System.out.println(c);
+	}
+
+}
---
+++ evenodd11
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class EvenOdd11
+{
+public static void main(String[] abcd)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("please enter number");
+
+int number = sc.nextInt();
+
+if(number%2 == 0)
+{
+System.out.println(number + " is an Even number");
+}
+else
+{
+System.out.println(number + " is an ODD number");
+
+}	
+}
+}
